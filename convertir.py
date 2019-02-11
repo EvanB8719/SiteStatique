7c1edba81534dc56edc317d6e35fe78fcaaf54ae
Baudier <evan.baudier@ynov.com>
2019-02-11 13:30:32 +0100

    test réussi

--- convertir.py
+++ convertir.py
@@ -1,31 +1,28 @@
+# coding=UTF-8
 import click
 import markdown2
 
 @click.command()
-@click.option('-i', help='chemin du fichier à convertir.')
-@click.option("-o", help = 'Chemin du fichier de sortie')
-# @click.option("-k", "--kikoolol", default = False, help = "Si c'est vrai ajouter un mot kikoo")
+@click.option('-i',"--input-file","input_file", default='', help='chemin du fichier à convertir.')
+@click.option("-o","--output-directory","output_directory", default='', help = 'Chemin du fichier de sortie')
+# @click.option("-k", "--kikoolol", default = False, help = "")
 
-def convertir (i, o):
+def convertir (input_file, output_directory):
+        ifile = input_file
+        od = output_directory
+        html_code_head = (
+            '<!DOCTYPE html>\n<html>\n<head>\n<meta charset="UTF-8">\n<title>Titre</title>\n</head>\n<body>\n'
+        )
+        html_code_foot = "</body>\n</html>"
+       
+        html = markdown2.markdown_path(ifile)
 
-    f = open(i, 'r', encoding='utf-8')
-    text2 = f.readlines()
-    f.close()
-    i = 0
-
-    for text1 in text2 :
-
-        markdown = text2 [i]
-        markdown = markdown.lstrip()
-        html = markdown2.markdown(markdown)
-        i += 1
-        page = open(o)
-
+        html_code = html_code_head + html + html_code_foot
+        page = open(od + "index.html", "w+", encoding="UTF-8").write(html_code)
 # # Bonus kikoolol
 #         if k == True:
 #                 listkikoo = ["<p>Kikoo</p>", "<p>lol</p>", "<p>mdr</p>", "<p>ptdr</p>"]
 #                 html.append(random.choice(listkikoo))
 #         print(html)
-    if __name__ == '__main__':
-
+if __name__ == '__main__':
         convertir()
